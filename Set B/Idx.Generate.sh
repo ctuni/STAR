58828126b4ece0f61a3432c2622523d78b1b745f
Cristina Tuñí i Domínguez <c.tuni.dominguez@gmail.com>
2019-05-22 18:18:20 +0200

    Initial commit

---
+++ Set B/Idx.Generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=10
+#SBATCH -p fast
+
+SCRIPT=/samscratch/Test_Cris_mouse/TFG/RawData/SetB/Idx.Generate
+
+singularity exec -B /samprojects:/samprojects -B /samscratch:/samscratch /samprojects/Dades_affymetrix/Singularity_images/star/container.img sh $SCRIPT
---
+++ Set B/STAR.alignment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=12
+#SBATCH -p long
+
+SCRIPT=/samscratch/Test_Cris_mouse/TFG/STAR/SetB/STAR.alignment
+
+singularity exec -B /samprojects:/samprojects -B /samscratch:/samscratch /samprojects/Dades_affymetrix/Singularity_images/star/container.img sh $SCRIPT
